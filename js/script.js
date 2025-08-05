584ad276b10effb5b9a262bea1f48a28ed031666
ARKHAN7863 <abdurrahmaankhan7863@gmail.com>
2025-08-05 22:32:46 +0200

    McLaren Supercars

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// toggle menu button
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+//Change the background video when click on the gallery images
+function changVideo(name) {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailers = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+    // javaScript higher order array function forEach
+    // this is easier to do data mapping
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+          video.classList.add('active');
+        }
+    });
+
+    trailers.forEach(trailer => {
+    trailer.classList.remove('active');
+    if (trailer.classList.contains(name)) {
+      trailer.classList.add('active');
+    }
+  });
+
+  models.forEach(model => {
+    model.classList.remove('active');
+    if (model.classList.contains(name)) {
+      model.classList.add('active');
+    }
+  });
+
+    // mapping model name change
+    models.forEach(model => {
+        model.classList.remove('active');
+        if (model.classList.contains(name)) {
+          model.classList.add('active');
+        }
+    });
+
+    // mapping trailer video change
+    trailers.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+          video.classList.add('active');
+        }
+    });
+}      
+
+// change the play and pause button on the click event
+function toggleplay(){
+    const play = document.querySelector('.play');
+    const pause = document.querySelector('.pause');
+    play.classList.toggle('active');
+    pause.classList.toggle('active');
+}
+
+//Video Play and Pause
+function pauseVideo() {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    bgVideoList.forEach(video => {
+        video.pause();
+    });
+    //
+    toggleplay();
+}
+
+function playVideo() {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    bgVideoList.forEach(video => {
+        video.play();
+    });
+
+    toggleplay();
+}
+
